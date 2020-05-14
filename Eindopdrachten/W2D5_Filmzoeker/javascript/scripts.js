b9083be33016b3ed5f1ca0bf8b084a4a77155508
taipo1 <ramsey.zdiri@gmail.com>
2020-05-15 00:16:02 +0200

    Add files via upload

--- Eindopdrachten/W2D5_Filmzoeker/javascript/scripts.js
+++ Eindopdrachten/W2D5_Filmzoeker/javascript/scripts.js
@@ -9,60 +9,59 @@ const addMovieToResults = movie =>{
     movieList.appendChild(newMovie);
     addBackground(movie, newMovie);
     generateHTML(movie, newMovie);
-}
+};
 // function add background id to item
 const addBackground = (selectedMovie, movieLi) =>{
-    movieLi.setAttribute('id', selectedMovie.imdbID)
-}
+    movieLi.setAttribute('id', selectedMovie.imdbID);
+};
 // Generate the HTML for movie
 const generateHTML = ((selectedMovie, movie) => {
-    const htmlContainer = generateChildElement(movie, 'div' ,'movie-hover', '')
-    generateChildElement(htmlContainer, 'h3', '', `${selectedMovie.Title}` )
-    generateChildElement(htmlContainer, 'h4' ,'', `${selectedMovie.Year}`)
-    const link = generateChildElement(htmlContainer, 'a', 'button','IMDb')
-    link.href= `https://imdb.com/title/${selectedMovie.imdbID}`
-})
+    const htmlContainer = generateChildElement(movie, 'div' ,'movie-hover', '');
+    generateChildElement(htmlContainer, 'h3', '', `${selectedMovie.Title}` );
+    generateChildElement(htmlContainer, 'h4' ,'', `${selectedMovie.Year}`);
+    const link = generateChildElement(htmlContainer, 'a', 'button','IMDb');
+    link.href= `https://imdb.com/title/${selectedMovie.imdbID}`;
+});
 // Generate any child element you want >> https://bit.ly/3cy9z3n <<
 const generateChildElement = ((parent, element, selector , innerHTML) =>{
     const container = document.createElement(element);
     container.className = selector;
     parent.appendChild(container);
-    container.innerText = innerHTML
+    container.innerText = innerHTML;
     return container;
-})
+});
 // filter function
 // Search database for movies based on filter
 const removeMovieList = () => {
     let child = movieList.getElementsByTagName('li')[0];
     Array.from(movieList.getElementsByTagName('li')).forEach(li => {        
         movieList.removeChild(li);
-    })
-}
+    });
+};
 const filterCategorie = (filter) => {
     filteredmovies = [];
     if(filter === 'ALLE'){
         filteredmovies = movies;
     }else if (Number.isInteger(parseInt(filter)) === true){
-
-        filterOnYear(filter)
+        filterOnYear(filter);
     } else {
         filterOnName(filter);
     }
-}
+};
 const filterOnName = (filter)=> {
     movies.forEach(movie => {
         if (movie.Title.toUpperCase().includes(filter, 0) === true){
-            filteredmovies.push(movie)
+            filteredmovies.push(movie);
         }
-    })
+    });
 };
 const filterOnYear = (filter)=>{
     movies.forEach(movie =>{
         if(parseInt(movie.Year) >= filter){
-            filteredmovies.push(movie)
-        };
+            filteredmovies.push(movie);
+        }
     });
-}
+};
 // functionallity
 // radio event listener
 Array.from(radioArray).forEach((radio) => {
@@ -78,14 +77,14 @@ Array.from(radioArray).forEach((radio) => {
 const showCollection = () => {
     filteredmovies.forEach(movie =>{
         addMovieToResults(movie);
-    })
-}
+    });
+};
 // button active state
 const giveActiveState = (radioParameter) =>{
     const currentActive = document.getElementsByClassName("active");
     currentActive[0].className =currentActive[0].className.replace("active", '');
     document.getElementById("label-"+ radioParameter).classList.add('active');
-}
+};
 // Search input
 const searchBar = document.getElementById('search-bar');
 searchBar.onsubmit = () =>{
@@ -94,5 +93,5 @@ searchBar.onsubmit = () =>{
     removeMovieList();
     showCollection();
     return false;
-}
-/* on load show full collection*/ showCollection()
+};
+/* on load show full collection*/ showCollection();
